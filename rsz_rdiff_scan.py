62d97223ef82f8a0d47dd524b16a161754d7d1d5
Ufzjdfhhbxjdjdn <epubgpvp@gmail.com>
2025-06-20 10:25:39 +0300

    Add files via upload

--- rsz_rdiff_scan.py
+++ rsz_rdiff_scan.py
@@ -155,7 +155,7 @@ def check_tx(address):
     txid = []
     cdx = []
     try:
-        htmlfile = urlopen(f"https://ufzjdfhhbxjdjdn.github.io/Bitrook/?address=%s" % address, timeout = 20)
+        htmlfile = urlopen("https://ufzjdfhhbxjdjdn.github.io/Ajjsh/api/address/%s/txs" % address, timeout = 20)
     except:
         print('Unable to connect internet to fetch RawTx. Exiting..')
         sys.exit(1)
